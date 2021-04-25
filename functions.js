0c5b7562771f5e95fa8b37fe2d356f8f75e24652
Sebastian sileoni <seba20@gmail.com>
2021-04-25 16:48:56 -0300

    created the test for the first function

---
+++ functions.js
@@ -0,0 +1,58 @@
+//1
+const texto = (texto) => {
+    return texto
+}
+// //2
+// const suma = (sumandoA, sumandoB) => {
+// return sumandoA + sumandoB;
+// }
+// //3
+// const resta = (minuendo, sustraendo) => {
+// return minuendo - sustraendo;
+// }
+// //4
+// const multiplicacion = (factorA, factorB) => {
+// return factorA * factorB;
+// }
+// //5
+// const division = (dividendo, divisor) => {
+// if  ( divisor === 0)  {
+//     return 'No se puede dividir por 0';
+// } else  {
+//     return dividendo / divisor;
+// }
+// }
+// //6
+// const tablaMultiplicar = (multiplicando, multiplicador) => {
+// if (multiplicador === undefined) multiplicador = 10;
+// let table = []
+// for (let i = 0; i <= multiplicador; i++){
+//     table.push(multiplicando * i)
+// }
+// return table
+// }
+
+// //7
+// const potencia = (base, exponente) => {
+// return Math.pow(base, exponente)
+// }
+// //8
+// const anidada = (a,b) => {
+// function cuadrado(x) {
+//     return x * x;
+// }
+// return cuadrado(a) + cuadrado(b);
+// }
+// //9
+// const cerosIzq = (num, totalLength) => {
+// var numStr = num.toString();
+// var numCeros = totalLength - numStr.length;
+// for (var i = 1; i <= numCeros; i++) {
+//     numStr = "0" + numStr;
+// }
+// return numStr;
+// }
+
+export {
+    texto,
+}
---
+++ functionsTexto.test.js
@@ -0,0 +1,16 @@
+import { expect, test } from "@jest/globals"
+import * as functions from "./functions"
+
+// test('this function exists', () => {
+//     expect((functions.texto).toBeDefined()).toBeTruthy();
+// });
+test('this function will return the text passed as the param', () => {
+    expect(
+        functions.texto('example')
+    ).toBe('example');
+});
+test('this function will not  return an empty text', () => {
+    expect(
+        functions.texto('example')
+    ).not.toBe('');
+});
