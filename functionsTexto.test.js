53f0aeadd195338a24d4b526ad5d1bda1774feed
Sebastian sileoni <seba20@gmail.com>
2021-04-25 17:54:12 -0300

    created the test for the second function SUMA

--- functionsTexto.test.js
+++ functionsTexto.test.js
@@ -4,11 +4,13 @@ import * as functions from "./functions"
 // test('this function exists', () => {
 //     expect((functions.texto).toBeDefined()).toBeTruthy();
 // });
+/*positive*/
 test('this function will return the text passed as the param', () => {
     expect(
         functions.texto('example')
     ).toBe('example');
 });
+/*negative*/
 test('this function will not  return an empty text', () => {
     expect(
         functions.texto('example')
